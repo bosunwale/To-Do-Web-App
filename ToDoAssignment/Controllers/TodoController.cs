e278dfeb9361a7236cc921938b2919ace2b5562f
johnthebeloved <johnoluwale1@gmail.com>
2022-08-04 16:33:35 -0400

    Create TodoController.cs

---
+++ ToDoAssignment/Controllers/TodoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoAssignment.Data;
+using ToDoAssignment.Models;
+
+namespace ToDoAssignment.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _todoContext;
+
+        public TodoController(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+
+        //GET: List of todo
+        public async Task<IActionResult> Index()
+        {
+            return View(await _todoContext.todo.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var todoList = await _todoContext.todo.
+                FirstOrDefaultAsync(m => m.Id == id);
+            if(todoList == null)
+            {
+                return NotFound();
+            }
+            return View(todoList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: ToDo/CreateToDo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,CreatedDate,IsCompleted")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                _todoContext.Add(todo);
+                await _todoContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+    }
+}
