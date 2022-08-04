0216b68f014fc3cf30a2a057547c5cb19b98c7b7
johnthebeloved <johnoluwale1@gmail.com>
2022-08-04 12:43:22 -0400

    Initial Commit

---
+++ ToDoAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
