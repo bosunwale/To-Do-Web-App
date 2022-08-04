b4b83bf54f2c5ff3fa000790153304522646f9db
johnthebeloved <johnoluwale1@gmail.com>
2022-08-04 12:54:29 -0400

    Create TodoContext.cs

---
+++ ToDoAssignment/Data/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAssignment.Models;
+
+namespace ToDoAssignment.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> todo { get; set; }
+    }
+}
