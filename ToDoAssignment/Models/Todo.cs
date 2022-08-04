124ab727057bb6a1c61e289b9c2d3b0d3e70fdc6
johnthebeloved <johnoluwale1@gmail.com>
2022-08-04 12:49:22 -0400

    Create Todo.cs

---
+++ ToDoAssignment/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoAssignment.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
